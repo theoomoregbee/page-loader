f789e11a8950eabe3026ca860cc71324047ade3a
Theophilus Omorebee <theo4u@ymail.com>
2017-09-23 16:56:38 +0100

    pages added

---
+++ src/app/page1/page1.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page1',
+  template: `
+    <p>
+      page1 Works!
+    </p>
+  `,
+  styles: []
+})
+export class Page1Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/page2/page2.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page2',
+  template: `
+    <p>
+      page2 Works!
+    </p>
+  `,
+  styles: []
+})
+export class Page2Component implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
