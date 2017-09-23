421a8c450e651d516281261ca3a267d1274e7d55
Theophilus Omorebee <theo4u@ymail.com>
2017-09-23 20:28:36 +0100

    done with navigation events

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SlimLoadingBarService} from 'ng2-slim-loading-bar';
+import {NavigationCancel, Event, NavigationEnd, NavigationError, NavigationStart, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,32 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private _loadingBar: SlimLoadingBarService, private _router: Router) {
+    this._router.events.subscribe((event: Event) => {
+      this.navigationInterceptor(event);
+    });
+  }
+
+  /**
+   * This is used to intercept and show Loading bar based on the current state of our
+   * Router navigation
+   * @param {Event} event
+   */
+  private navigationInterceptor(event: Event): void {
+    if (event instanceof NavigationStart) {
+      this._loadingBar.start();
+    }
+    if (event instanceof NavigationEnd) {
+      this._loadingBar.complete();
+    }
+
+    // Set loading state to false in both of the below events to hide the loader in case a request fails
+    if (event instanceof NavigationCancel) {
+      this._loadingBar.stop();
+    }
+    if (event instanceof NavigationError) {
+      this._loadingBar.stop();
+    }
+  }
 }
